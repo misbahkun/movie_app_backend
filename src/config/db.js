f217db9265c41c0f23c7a5cfa0d488c7df58adb2
Misbahudin <mizzc0d3@gmail.com>
2025-09-16 16:53:06 +0700

    fix: add client options for MongoDB connection and handle development proxy settings

--- src/config/db.js
+++ src/config/db.js
@@ -4,11 +4,23 @@ import { ENV_VARS } from './envVars.js';
 export const connectDB = async () => {
 	try {
 		const uri = process.env.NODE_ENV === 'test' ? globalThis.__MONGO_URI__ : ENV_VARS.MONGO_URI;
+
+		const clientOptions = {
+			serverApi: { version: '1', strict: true, deprecationErrors: true },
+		};
+
+		if (ENV_VARS.NODE_ENV == "development-proxy") {
+			clientOptions.proxyHost = '127.0.0.1'
+			clientOptions.proxyPort = 10808
+		}
+
 		if (!uri) {
 			throw new Error('MongoDB URI is not defined');
 		}
+
 		console.log('Connecting to MongoDB URI:', uri);
-		const conn = await mongoose.connect(uri);
+		const conn = await mongoose.connect(uri, clientOptions);
+
 		console.log('MongoDB connected: ' + conn.connection.host);
 	} catch (error) {
 		console.error('Error connecting to MongoDB: ' + error.message);
