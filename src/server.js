900ec9f63d9b59bc5ecffcd3d4ff2e33f2de834d
Misbahudin <mizzc0d3@gmail.com>
2025-09-16 17:08:38 +0700

    feat: add Docker configuration files and update server settings for improved deployment

--- src/server.js
+++ src/server.js
@@ -3,9 +3,11 @@ import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 
 const PORT = ENV_VARS.PORT;
+const HOST = '0.0.0.0';
 
-app.listen(PORT, () => {
-	console.log(`Environment: ${ENV_VARS.NODE_ENV}`)
-	console.log("Server started at http://localhost:" + PORT);
+app.listen(PORT, HOST, () => {
+	console.log(`Environment: ${ENV_VARS.NODE_ENV}`);
+	console.log(`Server started and listening on ${HOST}:${PORT}`);
+	console.log(`Access locally at http://localhost:${PORT}`);
 	connectDB();
 });
